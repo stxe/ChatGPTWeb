7958439b3ea6012405c3aa19abab044f596d4d4f
stxe <117729938+stxe@users.noreply.github.com>
2023-05-16 12:02:15 +0800

    update ads_txt.vue

--- src/views/ads_txt.vue
+++ src/views/ads_txt.vue
@@ -1,27 +1,6 @@
 <template>
 <div>aaaa
-<pre>{{ fileContents }}</pre>
+<pre></pre>
 </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-data() {
-return {
-fileContents: ""
-};
-},
-mounted() {
-this.getFileContents();
-},
-methods: {
-async getFileContents() {
-const response = await fetch("/assets/ads.txt");
-const text = await response.text();
-this.fileContents = text;
-}
-}
-});
-</script>
